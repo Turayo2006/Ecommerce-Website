118a04eab599fe46b7b3e99c956b1d2a00a8ddbd
Turayo2006 <120662177+Turayo2006@users.noreply.github.com>
2023-09-27 18:43:40 +0100

    Create shop-data.js

---
+++ shop-data.js
@@ -0,0 +1,94 @@
+let products = [
+    {
+        id: "a",
+        img: "https://i.postimg.cc/kg9YYbTn/f1.jpg",
+        name: "Carton Astronault Tshirts",
+        price: 78,
+        desc: ""
+    },
+    {
+        id: "b",
+        img: "https://i.postimg.cc/2yhT2kvb/f2.jpg",
+        name: "Carton Leave Tshirts",
+        price: 120,
+        desc: ""
+    },
+    {
+        id: "c",
+        img: "https://i.postimg.cc/VL9DtNm2/f3.jpg",
+        name: "Rose Multicolor Tshirts",
+        price: 180,
+        desc: ""
+    },
+    {
+        id: "d",
+        img: "https://i.postimg.cc/vZ3hPS1z/f4.jpg",
+        name: "Pink Flower Tshirt",
+        price: 250,
+        desc: ""
+    },
+    {
+        id: "e",
+        img: "https://i.postimg.cc/q7FLrhx6/f5.jpg",
+        name: "Purple Flowering Tshirts",
+        price: 380,
+        desc: ""
+    },
+    {
+        id: "f",
+        img: "https://i.postimg.cc/L86BZByZ/f7.jpg",
+        name: "Short Knicker",
+        price: 460,
+        desc: ""
+    },
+
+    {
+        id: "g",
+        img: "https://i.postimg.cc/zDxJ2f0H/f6.jpg",
+        name: "2 in 1 Double Routed",
+        price: 460,
+        desc: ""
+    },
+
+    {
+        id: "h",
+        img: "https://i.postimg.cc/x8qcBrpP/n6.jpg",
+        name: "Ash Short",
+        price: 460,
+        desc: ""
+    },
+    {
+        id: "abc",
+        img: "https://i.postimg.cc/2Sq4mytJ/f8.jpg",
+        name: "Carton Astronault Tshirts",
+        price: 78,
+        desc: ""
+    },
+
+    {
+        id: "bca",
+        img: "https://i.postimg.cc/KYjcC3sk/n3.jpg",
+        name: "Carton Leave Tshirts",
+        price: 120,
+        desc: ""
+    },
+
+    {
+        id: "cba",
+        img: "https://i.postimg.cc/vHvQBtJx/n4.jpg",
+        name: "Rose Multicolor Tshirts",
+        price: 180,
+        desc: ""
+    },
+
+    {
+        id: "dca",
+        img: "https://i.postimg.cc/908J8S4q/n5.jpg",
+        name: "Pink Flower Tshirt",
+        price: 250,
+        desc: ""
+    },
+
+
+
+]
