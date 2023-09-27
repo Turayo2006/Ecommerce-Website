3c24ac3962310b8d30cfe86cb117129f183932dd
Turayo2006 <120662177+Turayo2006@users.noreply.github.com>
2023-09-27 18:44:49 +0100

    Create data2.js

---
+++ data2.js
@@ -0,0 +1,35 @@
+let newItemsData = [
+    {
+        id: "abc",
+        img: "https://i.postimg.cc/2Sq4mytJ/f8.jpg",
+        name: "Carton Astronault Tshirts",
+        price: 78,
+        desc: ""
+    },
+
+    {
+        id: "bca",
+        img: "https://i.postimg.cc/KYjcC3sk/n3.jpg",
+        name: "Carton Leave Tshirts",
+        price: 120,
+        desc: ""
+    },
+
+    {
+        id: "cba",
+        img: "https://i.postimg.cc/vHvQBtJx/n4.jpg",
+        name: "Rose Multicolor Tshirts",
+        price: 180,
+        desc: ""
+    },
+
+    {
+        id: "dca",
+        img: "https://i.postimg.cc/908J8S4q/n5.jpg",
+        name: "Pink Flower Tshirt",
+        price: 250,
+        desc: ""
+    },
+
+
+]
