dd3413121e0e412d63da2925262935d80a6684ad
Tunrayo <120662177+Turayo2006@users.noreply.github.com>
2023-11-25 14:06:15 +0100

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -45,11 +45,23 @@ let generateOrderSummary = () => {
   generateOrderSummary();
 
 
+let TotalAmount = ()=>{
+    if (basket.length !== 0){
+        let amount = basket.map((x) =>{
+            let {item, id} = x;
+            let search = products.find((y) => y.id === id);  
+            return item * search.price;
+        })
+        .reduce((x,y) => x+y, 0);
+        // console.log(amount)
+        totalAmount.innerHTML = `
 
  <tr>
         <td><strong>Total</strong></td>
         <td><strong>$ ${amount}</strong></td>
-      </tr>
+      </tr>`
+
+        TotalAmount();
 
 
  
